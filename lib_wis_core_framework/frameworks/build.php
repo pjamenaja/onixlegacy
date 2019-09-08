ca868579a4199a9904b9f841b32f13eda15e8943
pjamenaja <pjame.fb@gmail.com>
2019-09-09 06:25:54 +0700

    POC move to docker

--- lib_wis_core_framework/frameworks/build.php
+++ lib_wis_core_framework/frameworks/build.php
@@ -4,6 +4,6 @@ Purpose : Auto generated built time file (DO NOT MODIFY)
 Created By : Seubpong Monsar
 */
 
-$ONIX_CORE_BUILT_DATE = '12/24/2018 08:17:27pm';
+$ONIX_CORE_BUILT_DATE = '09/09/2019 06:24:56am';
 
 ?>
