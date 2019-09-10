e9589b4924698ebbb5af68297fd8ff5fea4be4b7
pjamenaja <pjame.fb@gmail.com>
2019-09-10 20:55:58 +0700

    Added php

--- lib_wis_core_framework/frameworks/build.php
+++ lib_wis_core_framework/frameworks/build.php
@@ -4,6 +4,6 @@ Purpose : Auto generated built time file (DO NOT MODIFY)
 Created By : Seubpong Monsar
 */
 
-$ONIX_CORE_BUILT_DATE = '09/10/2019 08:25:18pm';
+$ONIX_CORE_BUILT_DATE = '09/10/2019 08:54:53pm';
 
 ?>
--- lib_wis_erp_framework/frameworks/build.php
+++ lib_wis_erp_framework/frameworks/build.php
@@ -4,6 +4,6 @@ Purpose : Auto generated built time file (DO NOT MODIFY)
 Created By : Seubpong Monsar
 */
 
-$ONIX_ERP_FRAMEWORK_BUILT_DATE = '09/10/2019 08:25:37pm';
+$ONIX_ERP_FRAMEWORK_BUILT_DATE = '09/10/2019 08:55:25pm';
 
 ?>
