18ff495561131ebbf351c3093b266e1489efadc6
dmp-devops <seubpong.mon@truedigital.group>
2020-06-17 20:58:08 +0700

    Redeploy

--- lib_wis_erp_framework/frameworks/build.php
+++ lib_wis_erp_framework/frameworks/build.php
@@ -4,6 +4,6 @@ Purpose : Auto generated built time file (DO NOT MODIFY)
 Created By : Seubpong Monsar
 */
 
-$ONIX_ERP_FRAMEWORK_BUILT_DATE = '06/17/2020 08:08:51pm';
+$ONIX_ERP_FRAMEWORK_BUILT_DATE = '06/17/2020 08:57:40pm';
 
 ?>
